bfbfbc29c7066071cc0629d9625dcba7d61eb2da
DavidPensjo <david.pensjo@gmail.com>
2024-05-07 20:33:36 +0200

    Add Radix UI components for avatar, separator, and scroll area

--- frontend/src/components/ChatPageComponents/MyChats.jsx
+++ frontend/src/components/ChatPageComponents/MyChats.jsx
@@ -1,8 +1,55 @@
 import React from "react";
 import { ChatState } from "@/Context/ChatProvider";
+import ChatPreview from "./MyChatsComponents/ChatPreview";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+
 const MyChats = () => {
   const { user } = ChatState();
-  return <div>welcome {user.userName}</div>;
+  return (
+    <div className="bg-[#494959] h-[760px] w-[380px] rounded-[8px] flex flex-col">
+      <div className="flex w-[380px] justify-center gap-4 pt-4">
+        <div className="flex ">
+          <input
+            className="w-[150px] h-[40px] bg-[#2B2B3C] border-[#2B2B3C] text-[#CFDBEC] rounded-l-[10px]"
+            placeholder="Search..."
+          ></input>
+          <button className="w-[40px] h-[40px] bg-[#2B2B3C] border-[#5E6875] border-2 text-[#CFDBEC] rounded-r-[10px]">aa</button>
+        </div>
+        <Button className="w-[120px] bg-[#2B2B3C]">+ Group Chat</Button>
+      </div>
+      <div className="flex flex-col pt-8 items-center h-[610px]">
+        <ScrollArea className="flex flex-col gap-5 w-[338px]">
+          <ChatPreview />
+          <Separator className="my-2.5 bg-[#494959]" />
+          <ChatPreview />
+          <Separator className="my-2.5 bg-[#494959]" />
+          <ChatPreview />
+          <Separator className="my-2.5 bg-[#494959]" />
+          <ChatPreview />
+          <Separator className="my-2.5 bg-[#494959]" />
+          <ChatPreview />
+          <Separator className="my-2.5 bg-[#494959]" />
+          <ChatPreview />
+          <Separator className="my-2.5 bg-[#494959]" />
+          <ChatPreview />
+          <Separator className="my-2.5 bg-[#494959]" />
+        </ScrollArea>
+      </div>
+      <div className="flex flex-row pt-6 pl-6">
+        <Avatar className="cursor-pointer">
+          <AvatarImage src="/pepe.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <a className="text-[#94A3B8] font-bold text-2xl pl-3 pt-1 underline cursor-pointer">
+          OfficialDavve
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default MyChats;
---
+++ frontend/src/components/ChatPageComponents/MyChatsComponents/ChatPreview.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+const ChatPreview = () => {
+  return (
+    <div className="h-[80px] w-[330px] bg-[#676773] rounded-xl grid grid-cols-7 grid-rows-3">
+      <div className="col-start-1 row-start-2 flex items-center justify-center pl-3">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+      <p className="text-white font-extrabold text-base col-start-2 row-start-1 col-span-3 pt-1 pl-4">
+        Big Steve
+      </p>
+      <p className="text-[#CFDBEC] text-sm col-start-2 row-start-2 col-span-5 pl-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+      <p className="text-[#CFDBEC] text-sm col-start-7 row-start-1 pt-1">07:54</p>
+    </div>
+  );
+};
+
+export default ChatPreview;
