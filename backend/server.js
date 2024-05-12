233051eb0c6bde6b80cb8b396ecb38dc74449160
DavidPensjo <david.pensjo@gmail.com>
2024-05-12 09:00:52 +0200

    Update CORS origin in server.js to http://localhost:5000

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ const server = app.listen(PORT, () =>
 const io = new SocketIOServer(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5000",
   },
 });
 
