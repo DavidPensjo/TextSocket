eb09ea9d30cd25eee13e227f8c7ce24086649f8a
DavidPensjo <david.pensjo@gmail.com>
2024-06-01 06:56:38 +0200

    Nested<p> tags fix

--- frontend/src/components/ChatPageComponents/MyChatsComponents/ChatPreview.jsx
+++ frontend/src/components/ChatPageComponents/MyChatsComponents/ChatPreview.jsx
@@ -24,25 +24,23 @@ const ChatPreview = ({ selectedChat, chat, loggedUser, user }) => {
               src={otherParticipant.picture || "defaultAvatar.webp"}
             />
           ) : (
-            <AvatarFallback className="font-semibold pb-0.5">
-              {chat.isGroupChat ? "G" : "U"}
-            </AvatarFallback> // 'G' for Group, 'U' for Undefined
+            <AvatarFallback className="font-semibold pb-0.5">{chat.isGroupChat ? "G" : "U"}</AvatarFallback> // 'G' for Group, 'U' for Undefined
           )}
         </Avatar>
       </div>
       <p className="text-white font-extrabold text-base col-start-2 row-start-1 col-span-4 pt-1 pl-4">
         {!chat.isGroupChat ? getSender(loggedUser, chat) : chat.chatName}
       </p>
-      <p className="text-[#CFDBEC] text-sm col-start-2 row-start-2 col-span-5 pl-4">
+      <div className="text-[#CFDBEC] text-sm col-start-2 row-start-2 col-span-5 pl-4">
         {chat.latestMessage && (
-          <p fontSize="xs">
+          <span style={{ fontSize: "small" }}> {/* Changed <p> to <span> */}
             <b>{chat.latestMessage.sender.userName} : </b>
             {chat.latestMessage.content.length > 50
-              ? chat.latestMessage.content.substring(0, 51) + "..."
+              ? `${chat.latestMessage.content.substring(0, 51)}...`
               : chat.latestMessage.content}
-          </p>
+          </span>
         )}
-      </p>
+      </div>
       <p className="text-[#CFDBEC] text-sm col-start-7 row-start-1 pt-1">
         {chat.latestMessage &&
           chat.latestMessage.createdAt
