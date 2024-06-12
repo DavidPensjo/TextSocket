09f1b2161cb1491fac206a592fa830814b9360c0
DavidPensjo <david.pensjo@gmail.com>
2024-06-12 23:34:06 +0200

    Update homepage layout and add lazy loading for images

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import LoginSignUpTabs from "@/components/HomePageComponents/LogInSignUpTabs";
 import TextSocketHome from "@/assets/TextSocketLogoBlack.png";
+
 const HomePage = () => {
   const history = useHistory();
 
@@ -12,12 +13,15 @@ const HomePage = () => {
   }, [history]);
 
   return (
-    <div className="font-roboto flex bg-black h-screen w-screen">
-      <div className="flex flex-col justify-center items-center w-full pb-28">
-        <h1 className=" text-7xl font-mono pb-10 text-white">TEXT SOCKET</h1>
+    <div className="font-roboto flex bg-black h-screen w-screen justify-center items-center relative">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-7xl font-mono pb-10 text-white">TEXT SOCKET</h1>
         <LoginSignUpTabs className="" />
       </div>
-      <img className="w-[800px] h-[800px]" src={TextSocketHome} />
+      <img
+        className="absolute right-0 w-72 h-72 md:w-96 md:h-96 lg:w-[500px] lg:h-[500px] xl:w-[750px] xl:h-[700px]"
+        src={TextSocketHome}
+      />
     </div>
   );
 };
