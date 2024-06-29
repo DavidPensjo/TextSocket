2c0f048312c5ed2b79948a6960898fee8d69a6d1
DavidPensjo <david.pensjo@gmail.com>
2024-06-29 19:31:09 +0200

    Adding ui to ChatDashboardDialog Todo: Make them function

--- frontend/src/components/ChatPageComponents/ChatBoxComponents/ChatDashboardDialog.jsx
+++ frontend/src/components/ChatPageComponents/ChatBoxComponents/ChatDashboardDialog.jsx
@@ -6,6 +6,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Users } from "lucide-react";
 import { ChatState } from "@/Context/ChatProvider";
@@ -24,9 +25,33 @@ const ChatDashboardDialog = () => {
         <Users className="h-[29px] w-[29px] mt-2" />
       </DialogTrigger>
       <DialogContent className="bg-[#494959] sm:max-w-[425px] sm:min-w-[425px] rounded flex flex-col items-center border-[#494959] overflow-y-auto max-h-[800px]">
-        <DialogHeader className="flex">
-          <DialogTitle className="text-[#CFDBEC]">Manage chat</DialogTitle>
-        </DialogHeader>
+        {selectedChat.isGroupChat && (
+          <div className="flex ">
+            <div className="text-[#CFDBEC] w-full flex">
+              <div className="relative inline-block h-[100px] w-[100px]">
+                <img
+                  className="h-full w-full rounded-full transition-transform duration-300 transform hover:scale-110 hover:opacity-80"
+                  src={selectedChat.groupPicture}
+                  alt="group picture"
+                />
+                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-70 rounded-full opacity-0 hover:opacity-100 transition-opacity duration-300">
+                  <span className="text-white pl-3 cursor-pointer">
+                    Change Icon
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="w-[250px] flex flex-col gap-0.5">
+              <p className="text-[#CFDBEC] font-bold">Group Name</p>
+              <Input
+                className=" bg-[#2B2B3C] border-[#2B2B3C] text-[#CFDBEC]"
+                type="text"
+                defaultValue={selectedChat.chatName}
+              />
+              <Button>Save Changes</Button>
+            </div>
+          </div>
+        )}
         <div className="text-[#CFDBEC] w-full">
           {chatUsers.map((chatUser) => (
             <UserListItem
@@ -39,20 +64,6 @@ const ChatDashboardDialog = () => {
             />
           ))}
         </div>
-        {selectedChat.isGroupChat && (
-          <div className="text-[#CFDBEC] w-full flex">
-            <div className="relative inline-block h-[80px] w-[80px]">
-              <img
-                className="h-full w-full rounded-full transition-transform duration-300 transform hover:scale-110 hover:opacity-80"
-                src={selectedChat.groupPicture}
-                alt="group picture"
-              />
-              <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-70 rounded-full opacity-0 hover:opacity-100 transition-opacity duration-300">
-                <span className="text-white pl-3 cursor-pointer">Change Icon</span>
-              </div>
-            </div>
-          </div>
-        )}
       </DialogContent>
     </Dialog>
   );
