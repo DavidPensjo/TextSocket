e4013a2bad2a439f7561eccb043f225c914a28de
DavidPensjo <david.pensjo@gmail.com>
2024-07-02 21:21:05 +0200

    Change groupName functionality

--- frontend/src/components/ChatPageComponents/ChatBoxComponents/ChatDashboardDialog.jsx
+++ frontend/src/components/ChatPageComponents/ChatBoxComponents/ChatDashboardDialog.jsx
@@ -11,14 +11,45 @@ import { Button } from "@/components/ui/button";
 import { Users } from "lucide-react";
 import { ChatState } from "@/Context/ChatProvider";
 import UserListItem from "../MyChatsComponents/UserListItem";
+import axios from "axios";
+import { useToast } from "@/components/ui/use-toast";
 
 const ChatDashboardDialog = () => {
-  const { user, selectedChat } = ChatState();
+  const { user, selectedChat, setSelectedChat } = ChatState();
   const [chatUsers, setChatUsers] = useState(selectedChat.users);
+  const [groupName, setGroupName] = useState(selectedChat.chatName);
+  const { toast } = useToast();
 
   const removeUserFromState = (userId) => {
     setChatUsers((prevUsers) => prevUsers.filter((u) => u._id !== userId));
   };
+
+
+  const handleSaveChanges = async () => {
+    try {
+      const { data } = await axios.put(
+        `/api/chat/rename`,
+        { chatId: selectedChat._id, chatName: groupName },
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setSelectedChat(data);
+      toast({
+        title: "Group name updated successfully",
+      });
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: "Failed to update group name.",
+      });
+    }
+  };
+
   return (
     <Dialog>
       <DialogTrigger>
@@ -26,29 +57,34 @@ const ChatDashboardDialog = () => {
       </DialogTrigger>
       <DialogContent className="bg-[#494959] sm:max-w-[425px] sm:min-w-[425px] rounded flex flex-col items-center border-[#494959] overflow-y-auto max-h-[800px]">
         {selectedChat.isGroupChat && (
-          <div className="flex ">
-            <div className="text-[#CFDBEC] w-full flex">
-              <div className="relative inline-block h-[100px] w-[100px]">
-                <img
-                  className="h-full w-full rounded-full transition-transform duration-300 transform hover:scale-110 hover:opacity-80"
-                  src={selectedChat.groupPicture}
-                  alt="group picture"
-                />
-                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-70 rounded-full opacity-0 hover:opacity-100 transition-opacity duration-300">
-                  <span className="text-white pl-3 cursor-pointer">
-                    Change Icon
-                  </span>
-                </div>
+          <div className="flex w-full p-4">
+            <div className="relative inline-block h-[100px] w-[100px] mr-4">
+              <img
+                className="h-full w-full rounded-full transition-transform duration-300 transform hover:scale-110 hover:opacity-80"
+                src={groupPicture}
+                alt="group picture"
+              />
+              <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-70 rounded-full opacity-0 hover:opacity-100 transition-opacity duration-300">
+                <label className="text-white pl-3 cursor-pointer">
+                  Change Icon
+                  <input
+                    type="file"
+                    accept="image/*"
+                    className="hidden"
+                    onChange={handlePictureChange}
+                  />
+                </label>
               </div>
             </div>
-            <div className="w-[250px] flex flex-col gap-0.5">
+            <div className="flex flex-col gap-0.5 w-full">
               <p className="text-[#CFDBEC] font-bold">Group Name</p>
               <Input
-                className=" bg-[#2B2B3C] border-[#2B2B3C] text-[#CFDBEC]"
+                className="bg-[#2B2B3C] border-[#2B2B3C] text-[#CFDBEC]"
                 type="text"
-                defaultValue={selectedChat.chatName}
+                value={groupName}
+                onChange={(e) => setGroupName(e.target.value)}
               />
-              <Button>Save Changes</Button>
+              <Button onClick={handleSaveChanges}>Save Changes</Button>
             </div>
           </div>
         )}
@@ -68,4 +104,5 @@ const ChatDashboardDialog = () => {
     </Dialog>
   );
 };
+
 export default ChatDashboardDialog;
