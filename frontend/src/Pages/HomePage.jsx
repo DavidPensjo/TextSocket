3abede3c5c316b414e86a785cf2a033f3f4bcdf7
DavidPensjo <david.pensjo@gmail.com>
2024-07-07 01:34:11 +0200

    Updated login page UI

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -14,19 +14,29 @@ const HomePage = () => {
 
   return (
     <div className="font-roboto flex bg-black h-screen w-screen justify-center items-center relative overflow-hidden">
-      <div className="flex justify-center items-center h-full w-full">
-        <div className="flex flex-col justify-center items-center relative z-10">
-          <h1 className="text-7xl font-mono pb-10 text-white">TEXT SOCKET</h1>
-          <LoginSignUpTabs className="" />
+      <div className="bg-black w-screen h-screen grid grid-cols-9 grid-rows-5">
+        <div className="col-span-2 row-span-2 row-start-1 flex justify-center items-center">
+          <img src={TextSocketHome} alt="Background" className="w-[350px]" />
         </div>
-        <div
-          className="absolute right-0 flex items-center justify-center h-full"
-        >
-          <img
-            src={TextSocketHome}
-            alt="Background"
-            className="object-contain h-1/2"
-          />
+        <div className="text-3xl col-span-1 row-span-1 col-start-3 row-start-3 ml-40 w-[400px]">
+          <p className="text-white">
+            This page hosts a real-time chat application called TEXTSOCKET
+          </p>
+          <ul className="text-white list-disc pl-8 pt-3 text-base">
+            <li>React</li>
+            <li>Tailwind CSS</li>
+            <li>MongoDB</li>
+            <li>AWS S3</li>
+            <li>Node.js and Express</li>
+            <li>JWT</li>
+            <li>Socket.io</li>
+          </ul>
+        </div>
+        <h1 className="col-span-3 text-7xl font-mono text-white col-start-3 row-start-1 mt-24 pl-36">TEXT SOCKET</h1>
+        <div className="row-span-2 bg-black col-start-6 row-start-3 pl-[40px]">
+          <div className="bg-black flex flex-col justify-center items-center">
+            <LoginSignUpTabs className="" />
+          </div>
         </div>
       </div>
     </div>
