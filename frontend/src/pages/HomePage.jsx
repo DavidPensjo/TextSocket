43ca1a5732aaf4068d0a795a68833190c1cbb961
DavidPensjo <david.pensjo@gmail.com>
2024-07-07 03:00:22 +0200

    Evervault login page implementation

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,13 @@
 import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import LoginSignUpTabs from "@/components/HomePageComponents/LogInSignUpTabs";
-import TextSocketHome from "@/assets/TextSocketLogoBlack.png";
+import { EvervaultCard } from "@/components/ui/evervault-card";
 
 const HomePage = () => {
   const history = useHistory();
 
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("userInfo"));
-
     if (user) history.push("/chats");
   }, [history]);
 
@@ -16,11 +15,11 @@ const HomePage = () => {
     <div className="font-roboto flex bg-black h-screen w-screen justify-center items-center relative overflow-hidden">
       <div className="bg-black w-screen h-screen grid grid-cols-9 grid-rows-5">
         <div className="col-span-2 row-span-2 row-start-1 flex justify-center items-center">
-          <img src={TextSocketHome} alt="Background" className="w-[350px]" />
+          <EvervaultCard className="h-[300px] w-[300px] 2xl:h-[400px] 2xl:w-[400px] xxl:h-[500px] xxl:w-[500px]" />
         </div>
-        <div className="text-3xl col-span-1 row-span-1 col-start-3 row-start-3 ml-40 w-[400px]">
+        <div className="text-3xl col-span-1 row-span-1 col-start-3 row-start-3 2xl:ml-40 w-[300px] 2xl:h-[350px] 2xl:w-[350px]">
           <p className="text-white">
-            This page hosts a real-time chat application called TEXTSOCKET
+            This page hosts a real-time chat application named TEXT SOCKET
           </p>
           <ul className="text-white list-disc pl-8 pt-3 text-base">
             <li>React</li>
@@ -32,8 +31,10 @@ const HomePage = () => {
             <li>Socket.io</li>
           </ul>
         </div>
-        <h1 className="col-span-3 text-7xl font-mono text-white col-start-3 row-start-1 mt-24 pl-36">TEXT SOCKET</h1>
-        <div className="row-span-2 bg-black col-start-6 row-start-3 pl-[40px]">
+        <h1 className="col-span-5 text-7xl font-mono text-white col-start-3 row-start-1 mt-24 pl-36">
+          TEXT SOCKET
+        </h1>
+        <div className="2xl:pl-20 pl-40 row-span-2 bg-black col-start-6 row-start-3">
           <div className="bg-black flex flex-col justify-center items-center">
             <LoginSignUpTabs className="" />
           </div>
